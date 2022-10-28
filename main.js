cf66c7756e8d70bcb87b3ba3cdd358b761019f5b
Ridaan904 <100933055+Ridaan904@users.noreply.github.com>
2022-10-28 16:37:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+function preload()
+{
+    mustache_lips = loadImage('https://i.postimg.cc/pTxZkytH/mustache-student-math-favorite-for-friday-the-the-1.png');
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 35;
+        noseY = results[0].pose.nose.y - 15;
+                console.log("nose_x = "+ noseX);
+        console.log("nose_y = "+ noseY);
+    }
+}
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache_lips, noseX, noseY, 70, 70);
+}
+function take_snapshot()
+{
+    save('myFilteredImg');
+}
